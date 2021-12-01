f65b3690fafa7fb423f4cd02d27aa49a2878d6e1
Nelson Chinedu <egwuchinedu69@gmail.com>
2021-12-01 06:56:12 +0100

    customer order preview

---
+++ src/graphql/resolvers/client/query/previewOrder.ts
@@ -0,0 +1,49 @@
+import winstonEnvLogger from 'winston-env-logger';
+import { ForbiddenError } from 'apollo-server';
+import { getRepository } from 'typeorm';
+
+import { Account, Order } from '../../../../db';
+
+import IContext from '../../../../interface/IContext';
+
+const previewOrder = async (
+  _parent: unknown,
+  args: { productId: string },
+  { user: { id } }: IContext
+) => {
+  const { productId } = args;
+  try {
+    const account = await getRepository(Account).findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!account) {
+      throw new ForbiddenError('Permission denied');
+    } else if (account && account.blocked) {
+      throw new ForbiddenError('Account blocked, kindly contact support');
+    }
+
+    const order: Order | undefined = await Order.findOne({
+      where: {
+        orderId: productId,
+      },
+      relations: ['product'],
+    });
+
+    if (!order) return null;
+
+    const { status, orderId, createdAt, product } = order;
+
+    return { status, orderId, createdAt, product };
+  } catch (error) {
+    winstonEnvLogger.error({
+      message: 'An error occured',
+      error,
+    });
+    throw error;
+  }
+};
+
+export default previewOrder;
